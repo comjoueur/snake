ebada562e03149f1a24703bbd9b22550dddc46ca
comjoueur <renatochavezurday820@gmail.com>
2017-05-13 10:09:27 -0500

    Update juego.py

--- juego.py
+++ juego.py
@@ -1,9 +1,9 @@
 from random import *
+from os import *
 class espacio:
     x=50
     y=50
 class serpiente:
-    tamano=0
     direccion="derecha"
     posicion=[]
     par=[0,0]
@@ -63,9 +63,34 @@ def aparecer_fruta(a,espacio,manzana):
         y=randrange(0,espacio.y)
     manzana.x=x
     manzana.y=y
+def crecer(a):
+    a.posicion.insert(0,a.posicion[0])
 def comer_fruta(a,espacio,manzana):
     a.puntaje+=10
     aparecer_fruta(a,espacio,manzana)
+    crecer(a)
 def p_comiofruta(a,espacio,manzana):
     l=[manzana.x,manzana.y]
     if(a.posicion[len(a.posicion)-1]==l):comer_fruta(a,espacio,manzana)
+def ganar(a):
+    if(a.puntaje>=100):
+        system("clear")
+        print "tu ganaste"
+def perder(a,espacio):
+    i=a.posicion[len(a.posicion)-1][0]
+    j=a.posicion[len(a.posicion)-1][1]
+    if(i<0 or i>=espacio.x or j<0 or j>=espacio.y):
+        system("clear")
+        print "perdiste"
+    else:
+        bool w=False
+        for i in range(len(a.posicion)-1)
+            if i==a[a.posicion-1]:w=True
+        if(w):
+            system("clear")
+            print "perdiste"
+        
+def estado(a,espacio,manzana):
+    p_comiofruta(a,espacio,manzana)
+    ganar(a)
+    perder(a,espacio)
